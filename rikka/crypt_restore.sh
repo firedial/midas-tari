350d0a7780acbb52e0a37d9abc02985d1278a0de
firedial <42674772+firedial@users.noreply.github.com>
2021-12-28 23:06:47 +0900

    バックアップとリストアを暗号化してできるようにした

---
+++ rikka/crypt_restore.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ENCRYPTED_BACKUP_FILE_NAME=$1
+
+openssl aes-256-cbc -d -pbkdf2 -iter 100000 -salt -in /tmp/${ENCRYPTED_BACKUP_FILE_NAME} -out /tmp/restore.dump.gz -pass file:pass.txt
+gunzip /tmp/restore.dump.gz
+
+/usr/bin/mysql -u ${DB_MIDAS_ROOT_USER} -p${DB_MIDAS_ROOT_PASS} -h ${DB_HOST} ${DB_DATABASE_NAME} < /tmp/restore.dump
+rm /tmp/restore.dump

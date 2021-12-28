b7950512e76643557c44aa5b1815c8047614a12c
firedial <42674772+firedial@users.noreply.github.com>
2021-12-28 23:49:48 +0900

    バックアップのファイル名を変更

--- rikka/crypt_backup.sh
+++ rikka/crypt_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BACKUP_FILE_NAME="backup$(date +\%Y\%m\%d\%H\%M\%S).dump"
+BACKUP_FILE_NAME="backup.dump"
 /usr/bin/mysqldump --no-tablespaces --single-transaction -u ${DB_MIDAS_BACKUP_USER} -p${DB_MIDAS_BACKUP_PASS} -h ${DB_HOST} ${DB_DATABASE_NAME} > /tmp/${BACKUP_FILE_NAME}
 gzip /tmp/${BACKUP_FILE_NAME}
 BACKUP_GZIP_FILE_NAME=${BACKUP_FILE_NAME}.gz
@@ -8,6 +8,7 @@ BACKUP_GZIP_FILE_NAME=${BACKUP_FILE_NAME}.gz
 openssl aes-256-cbc -e -pbkdf2 -iter 100000 -salt -in /tmp/${BACKUP_GZIP_FILE_NAME} -out /tmp/encrypted_${BACKUP_GZIP_FILE_NAME} -pass file:pass.txt
 
 rm /tmp/${BACKUP_GZIP_FILE_NAME}
-# rm /tmp/encrypted_${BACKUP_GZIP_FILE_NAME}
 
+# BACKUP_FILE_NAME="backup$(date +\%Y\%m\%d\%H\%M\%S).dump"
+# rm /tmp/encrypted_${BACKUP_GZIP_FILE_NAME}
 

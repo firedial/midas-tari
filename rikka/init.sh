02d88b5828738adb73e96c7708cc43906b0987e1
firedial <42674772+firedial@users.noreply.github.com>
2022-01-05 20:49:30 +0900

    マウントするだけのものにしあ

--- rikka/init.sh
+++ rikka/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mount -t cifs ${NAS_PATH} /mnt/nas -o username=${NAS_USER},password=${NAS_PASS},iocharset=utf8,rw,uid=0,gid=0
+mount -t cifs ${NAS_PATH} /mnt/nas -o username=${NAS_USER},password=${NAS_PASS},iocharset=utf8,rw,uid=1000,gid=1000
 
-/usr/sbin/crond
-crontab /home/root/crontab_setting
+# /usr/sbin/crond
+# crontab /home/root/crontab_setting
 
